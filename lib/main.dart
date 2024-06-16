78f3b3a7855592016802f4a33eda5b477886118b
iambjlu <iambjlu@gmail.com>
2024-06-16 05:08:03 +0000

    外觀修正

--- lib/main.dart
+++ lib/main.dart
@@ -21,49 +21,58 @@ class DiceScreen extends StatefulWidget {
 
 class _DiceScreenState extends State<DiceScreen> {
   String _diceResult = '?';
-  String _imagePath= 'assets/dicex.png';
+  String _imagePath = 'assets/dicex.png';
 
   void _rollDice() {
     HapticFeedback.mediumImpact();
     setState(() {
       _diceResult = (Random().nextInt(6) + 1).toString();
-      _imagePath='assets/dice'+_diceResult+'.png';
+      _imagePath = 'assets/dice$_diceResult.png';
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: const Text('骰子', style: TextStyle(fontFamily: "PingFang TC")),
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('骰子', style: TextStyle(fontFamily: "PingFang TC")),
       ),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset(_imagePath,height: 75,),
+            Image.asset(
+              _imagePath,
+              height: 75,
+            ),
             const SizedBox(height: 40),
             const Text(
               '骰子數字',
               style: TextStyle(
                 fontFamily: "PingFang TC",
                 fontSize: 24,
+                fontWeight: FontWeight.w500,
               ),
             ),
             Text(
-              '$_diceResult',
-              style: TextStyle(
+              _diceResult,
+              style: const TextStyle(
                 fontFamily: "SF Pro Display",
                 fontSize: 40,
+                fontWeight: FontWeight.w700,
               ),
             ),
             const SizedBox(height: 40),
             CupertinoButton.filled(
+              onPressed: _rollDice,
               child: const Text(
                 '丟骰子',
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: "PingFang TC",
+                    fontWeight: FontWeight.w500,
+                    color: Color.fromARGB(255, 255, 255, 255)),
               ),
-              onPressed: _rollDice,
             ),
           ],
         ),
